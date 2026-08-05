ef66d51f40cb1789130b9beecfa3d110629b2a60
Furogs <ismaelvatotoa757@gmail.com>
2026-08-05 08:20:15 +0300

    kaeru personal commit

--- Models/BankManager.cs
+++ Models/BankManager.cs
@@ -11,7 +11,7 @@ public interface IDatabaseConnection
 
 public sealed class DatabaseConnection : IDatabaseConnection
 {
-    private const string Owner = "Host=localhost;Database=bank;Username=bank_manager;Password=bankmanager";
+    private const string Owner = "Host=localhost;Database=bank;Username=bank_manager;Password=bankmanager;Timeout=15";
 
     private static readonly Lazy<DatabaseConnection> _instance = new (()=> new DatabaseConnection());
     
@@ -21,45 +21,13 @@ public sealed class DatabaseConnection : IDatabaseConnection
 
     private static NpgsqlConnection Connected()
     {
-        NpgsqlConnection Connected();
-        string TestConnection();
+        return new NpgsqlConnection(Owner);
     }
     
     public async Task<NpgsqlConnection> KaeruConnectAsync()
     {
-         private const string Owner = "Host=localhost;Database=bank;Username=bank_manager;Password=bankmanager;Timeout=3;";
-
-        //for antsa's pc
-        //private const string Owner = "Host=localhost;Database=bank;Username=postgres;Password=root;Timeout=3;";
-
-        private static readonly Lazy<DatabaseConnection> _instance = new Lazy<DatabaseConnection>(() => new DatabaseConnection());
-
-        public static IDatabaseConnection Instance => _instance.Value;
-
-        private DatabaseConnection() { }
-
-        private static NpgsqlConnection Connected()
-        {
-            return new NpgsqlConnection(Owner);
-        }
-
-        public string TestConnection()
-        {
-            try
-            {
-                using var connection = Connected();
-                connection.Open();
-
-                return ("Connexion à la base de données réussie !");
-            }
-            catch (NpgsqlException ex)
-            {
-                return ($"Échec de connexion PostgreSQL : {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                return ($"Une erreur inattendue est survenue : {ex.Message}");
-            }
-        }
+        NpgsqlConnection connection = Connected();
+        await connection.OpenAsync();
+        return connection;
     }
 }
