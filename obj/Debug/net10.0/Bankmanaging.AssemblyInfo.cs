1f4be71894828a1aa9882c09fd5191b8c41f2e0f
ErenxBKRT <rakotoarinelinalaza@gmail.com>
2026-08-08 16:12:05 +0300

    -

--- obj/Debug/net10.0/Bankmanaging.AssemblyInfo.cs
+++ obj/Debug/net10.0/Bankmanaging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86bb1d0a081c42f17fae261fcb2a5a300a4e480e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002f2ff67f85dbf33379101cd550687671ee6141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
