36edd16f98d5e33372cb763477a589c908c39fb5
Vat-Is <ismaelsan120m@gmail.com>
2026-08-08 20:51:01 +0300

    should work fine now

--- Models/Client.cs
+++ Models/Client.cs
@@ -2,17 +2,10 @@ namespace Bankmanaging.Models;
 
 public class Client 
 {
-    public int IdClient {get; set; }
+    public int Id {get; set; }
     public string Nom { get; set; } = string.Empty;
     public string Prenom { get; set; } = string.Empty;
     public string Adresse { get; set; } = string.Empty;
-<<<<<<< HEAD
-    public string? Contact { get; set; }
-    public decimal Solde { get; set; } = 0.00m;
-    public bool Bloque { get; set; } = false;
-    public decimal Credit { get; set; } = 0.00m;
-=======
     public string Contact { get; set; } = string.Empty;
     public bool Bloque { get; set; } = false;
->>>>>>> 1fc40be (...)
 }
