ad9589b8fb03aef61521a0ec724c3ab340b5c8cd
Vat-Is <ismaelsan120m@gmail.com>
2026-08-09 13:04:05 +0300

    -

--- obj/Debug/net10.0/Bankmanaging.AssemblyInfo.cs
+++ obj/Debug/net10.0/Bankmanaging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36edd16f98d5e33372cb763477a589c908c39fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c4feebff3690c3ccc26a5fae840d616fffe1ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bankmanaging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
