ce6a16a50903d2c91845e077698507cb9ded323e
Vat-Is <ismaelsan120m@gmail.com>
2026-08-10 13:49:39 +0300

    -

--- ViewModels/clientDetailViewModel.cs
+++ ViewModels/clientDetailViewModel.cs
@@ -2,7 +2,6 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.Input;
 using Bankmanaging.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Bankmanaging.ViewModels;
 
